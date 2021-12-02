19472bf967355b2c005c8f1a1f2bcc2735d69b28
Yael Atletl <yael.atletl@gmail.com>
2021-12-02 01:59:26 -0600

    Boton de inicio cambia escena

--- Main.gd
+++ Main.gd
@@ -11,4 +11,4 @@ func _on_Opciones_pressed() -> void:
 	add_child(Opciones.instance())
 
 func _on_Iniciar_pressed() -> void:
-	get_tree().change_scene(PrimeraEscena.instance())
+	get_tree().change_scene("res://Inicio.tscn")
