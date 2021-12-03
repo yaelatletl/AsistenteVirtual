7c024743f91e9df3ad7b3e1d36c537aa0c03ea4f
Yael Atletl <yael.atletl@gmail.com>
2021-12-02 21:57:13 -0600

    Cambios Caleb

---
+++ Label6.gd
@@ -0,0 +1,12 @@
+extends Label
+
+var page = 0
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		page += 1
+		
+		if page == 1:
+			set_text("¿Seguro?")
+		elif page == 2:
+				get_tree().change_scene("Inicio8.tscn")
