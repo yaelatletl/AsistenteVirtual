d1f860c35be8105402a09752ef3b5acb145a87cc
Yael Atletl <yael.atletl@gmail.com>
2021-12-02 23:13:40 -0600

    Volumen funciona

--- Main.gd
+++ Main.gd
@@ -3,6 +3,14 @@ extends Control
 export(PackedScene) var Opciones 
 export(PackedScene) var PrimeraEscena
 
+var volGeneral = 0
+var volMusic = 0
+var volVoice = 0
+var volDb = 0
+func _ready() -> void:
+	volDb = AudioServer.get_bus_peak_volume_left_db(0,0)
+	_load_config()
+
 func _on_Salir_pressed() -> void:
 	get_tree().quit()
 
@@ -12,3 +20,14 @@ func _on_Opciones_pressed() -> void:
 
 func _on_Iniciar_pressed() -> void:
 	get_tree().change_scene("res://Inicio.tscn")
+
+
+func _load_config():
+	var config = ConfigFile.new()
+	config.load("config.cfg")
+	volGeneral = config.get_value("general", "volume", 100)
+	volMusic = config.get_value("music", "volume", 100)
+	volVoice = config.get_value("voice", "volume", 100)
+	if volMusic == 0:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 2*(volMusic/100))
--- Opciones.gd
+++ Opciones.gd
@@ -3,12 +3,14 @@ extends Control
 var volGeneral = 0
 var volMusic = 0
 var volVoice = 0
+var volDb
 
 onready var volGeneralSlider  = $VBoxContainer/HBoxContainer/Panel/HBoxContainer/VBoxContainer/HBoxContainer/VolGeneral
 onready var volMusicSlider    = $VBoxContainer/HBoxContainer/Panel/HBoxContainer/VBoxContainer/HBoxContainer3/VolMusica
 onready var volVoiceSlider    = $VBoxContainer/HBoxContainer/Panel/HBoxContainer/VBoxContainer/HBoxContainer2/VolVoces
 
 func _ready() -> void:
+	volDb = AudioServer.get_bus_peak_volume_left_db(0,0)
 	_load_config()
 	volGeneralSlider.value = volGeneral
 	volMusicSlider.value = volMusic
@@ -16,17 +18,23 @@ func _ready() -> void:
 
 func _save_config():
 	var config = ConfigFile.new()
-	config.add_value("general", "volume", volGeneral)
-	config.add_value("music", "volume", volMusic)
-	config.add_value("voice", "volume", volVoice)
+	config.set_value("general", "volume", volGeneral)
+	config.set_value("music", "volume", volMusic)
+	config.set_value("voice", "volume", volVoice)
 	config.save("config.cfg")
+	if volMusic == 0:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 2*(volMusic/100))
 
 func _load_config():
 	var config = ConfigFile.new()
 	config.load("config.cfg")
-	volGeneral = config.get_value("general", "volume")
-	volMusic = config.get_value("music", "volume")
-	volVoice = config.get_value("voice", "volume")
+	volGeneral = config.get_value("general", "volume", 100)
+	volMusic = config.get_value("music", "volume", 100)
+	volVoice = config.get_value("voice", "volume", 100)
+	if volMusic == 0:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 2*(volMusic/100))
 
 func _on_Aceptar_pressed() -> void:
 	_save_config()
